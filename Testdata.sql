a67d0bfe8891dfa15efd74d38d4d7fc8dab40ea0
tookNyAina0907 <tookrandrina@gmail.com>
2026-03-08 13:51:10 +0300

    [script] [donne] *.sql [correction]

--- Testdata.sql
+++ Testdata.sql
@@ -1,42 +1,78 @@
-INSERT INTO t_candidat (nom, prenom, matricule) VALUES
-('Rakoto', 'Jean', 'MAT001'),
-('Rabe', 'Marie', 'MAT002'),
-('Andrianina', 'Paul', 'MAT003'),
-('Razanaka', 'Luc', 'MAT004'),
-('Rakotomalala', 'Sofia', 'MAT005');
 
-INSERT INTO t_correcteur (nom, prenom, numtel) VALUES
-('Rasoa', 'Andry', '0341234567'),
-('Rasoanaivo', 'Hery', '0342345678'),
-('Rajaonarison', 'Lala', '0343456789'),
-('Rakotondrazaka', 'Mamy', '0344567890'),
-('Ratsimbazafy', 'Fidy', '0345678901');
 
-INSERT INTO t_matiere (nom, coefficient) VALUES
-('Mathematiques', 4),
-('Physique', 3),
-('Chimie', 2),
-('Informatique', 5),
-('Biologie', 1);
+INSERT INTO t_candidat (id, nom, prenom, matricule) VALUES
+(1,'Rakoto','Jean','MAT001'),
+(2,'Rabe','Marie','MAT002'),
+(3,'Andrianina','Paul','MAT003');
 
-INSERT INTO t_resolution (nom) VALUES
-('plus petit'),
-('plus grand'),
-('moyenne');
 
-INSERT INTO t_operateur (operateur) VALUES
-('<'),
-('>'),
-('='),
-('<='),
-('>=');
+INSERT INTO t_correcteur (id, nom, prenom, numtel) VALUES
+(1,'Rasoa','Andry','341234567'),
+(2,'Rasoanaivo','Hery','342345678'),
+(3,'Rajaonarison','Lala','343456789'),
+(4,'Rakotondrazaka','Mamy','344567890'),
+(5,'Ratsimbazafy','Fidy','345678901'),
+(6,'Rakotomalala','Sofia','345664901');
 
-INSERT INTO t_parametre (id_matiere, id_resolution, id_operateur, seuil) VALUES
-(1, 1, 1, 10.0),
-(2, 2, 2, 12.0),
-(3, 3, 1, 8.0),
-(4, 1, 2, 15.0),
-(5, 2, 1, 5.0);
 
-INSERT INTO t_note (id_candidat, id_matiere, id_correcteur, note) VALUES
-(1,1,1,12.5)
+INSERT INTO t_matiere (id, nom, coefficient) VALUES
+(1,'Mathematiques',4),
+(2,'Physique',3),
+(3,'Chimie',2);
+
+
+INSERT INTO t_resolution (id, nom) VALUES
+(1,'plus petit'),
+(2,'plus grand'),
+(3,'moyenne');
+
+
+INSERT INTO t_operateur (id, operateur) VALUES
+(1,'<'),
+(2,'>'),
+(3,'=');
+
+
+
+INSERT INTO t_parametre (id,id_matiere,id_resolution,id_operateur,seuil) VALUES
+(1,1,1,1,8),
+(2,1,2,2,7),
+(3,1,3,3,2),
+(4,2,1,1,6),
+(5,2,2,2,3),
+(6,2,3,3,4),
+(7,3,1,1,12),
+(8,3,2,2,10),
+(9,3,3,3,16);
+
+
+INSERT INTO t_note (id_candidat,id_matiere,id_correcteur,note) VALUES
+(1,1,1,10.5),
+(1,1,2,9),
+(1,1,3,13),
+(1,2,4,12),
+(1,2,5,12),
+(1,2,6,12),
+(1,3,1,17),
+(1,3,2,9),
+(1,3,3,14),
+
+(2,1,4,8),
+(2,1,5,11),
+(2,1,6,9),
+(2,2,1,10),
+(2,2,2,12),
+(2,2,3,11),
+(2,3,4,13),
+(2,3,5,12),
+(2,3,6,14),
+
+(3,1,1,15),
+(3,1,2,14),
+(3,1,3,16),
+(3,2,4,9),
+(3,2,5,10),
+(3,2,6,11),
+(3,3,1,18),
+(3,3,2,17),
+(3,3,3,16);
